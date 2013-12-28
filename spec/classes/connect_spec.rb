72d2717bef337ac89b389141b623098b4f6f8648
Jonathan Webb <webbj2012@jwwebb.info>
2013-12-27 20:31:21 -0500

    Adds spec classes for new Fitbit device classes

---
+++ spec/classes/connect_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'fitbit::connect' do
+  it do
+    should contain_package('FitbitConnect').with({
+      :provider => 'pkgdmg',
+      :source   => 'http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_20130926_1.0.0.4012.dmg',
+    })
+  end
+end
