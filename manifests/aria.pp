2e4312f9d457620b5bb33ec932d76cef383bf68c
Jonathan Webb <webbj2012@jwwebb.info>
2013-12-27 20:33:49 -0500

    Fixes lint issue with package sources

--- manifests/aria.pp
+++ manifests/aria.pp
@@ -6,6 +6,6 @@
 class fitbit::aria {
   package { 'FitbitScale':
     provider => 'pkgdmg',
-    source   => "http://cache.fitbit.com/scaleSetup/FitbitScaleSetup_Mac_20121127_1.0.0.86.dmg"
+    source   => 'http://cache.fitbit.com/scaleSetup/FitbitScaleSetup_Mac_20121127_1.0.0.86.dmg'
   }
 }
--- manifests/connect.pp
+++ manifests/connect.pp
@@ -6,6 +6,6 @@
 class fitbit::connect {
   package { 'FitbitConnect':
     provider => 'pkgdmg',
-    source   => "http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_20130926_1.0.0.4012.dmg"
+    source   => 'http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_20130926_1.0.0.4012.dmg'
   }
 }
